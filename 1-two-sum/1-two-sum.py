5729942cded9deff1527123c39f6cd1f733122aa
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-05-03 17:11:44 -0700

    Time: 97 ms (54.51%), Space: 15.2 MB (50.06%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict_nums = {}
+        for index, item in enumerate(nums):
+            dict_nums[item] = index
+        
+        lst = []
+        
+        for index, num in enumerate(nums):
+            if target - num in dict_nums and dict_nums[target - num] != index:
+                lst.append(index)
+                lst.append(dict_nums[target - num])
+                return lst
+                
+        
+        
