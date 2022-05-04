8d84014cdbe6470fcd74cbe4a885c0a635d9cb31
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-05-03 17:56:17 -0700

    Time: 1064 ms (21.43%), Space: 27 MB (61.61%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,21 +1,26 @@
 class Solution:
-    def maxOperations(self, nums: List[int], k: int) -> int:
+    def maxOperations(self, numbers: List[int], target: int) -> int:
         dict_nums = {}
-        for number in nums:
-            if number in dict_nums:
-                dict_nums[number] += 1
+        for num in numbers:
+            if type(num) == int or type(num) == float:
+                if num in dict_nums:
+                    dict_nums[num] += 1
+                else:
+                    dict_nums.setdefault(num, 1)
             else:
-                dict_nums.setdefault(number, 1)
+                continue
+
         counter = 0
 
-        for number in nums:
-            if (k - number) in dict_nums:
-                counter += 1
-                if dict_nums[k - number] == 1:
-                    del dict_nums[k - number]
-                elif dict_nums[k - number] > 1:
-                    dict_nums[k - number] -= 1
-                    
-        return counter // 2
+        for num in numbers:
+            if type(num) == int or type(num) == float:
+                if (target - num) in dict_nums:
+                    counter += 1
+                    if dict_nums[target - num] == 1:
+                        del dict_nums[target - num]
+                    elif dict_nums[target - num] > 1:
+                        dict_nums[target - num] -= 1
+            else:
+                continue
 
-            
+        return counter // 2
