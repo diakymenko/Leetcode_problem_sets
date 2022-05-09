809a2fbd7cf268fa4320e77fc7f2f042cce89ee6
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-05-08 22:48:32 -0700

    Time: 67 ms (18.78%), Space: 14 MB (39.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -6,19 +6,16 @@ class Solution:
         to_place = len(nums1) - 1
 
         while to_place >= 0:
-            if left_pointer < 0:
+            if right_pointer< 0:
+                break
+            elif left_pointer < 0 or nums2[right_pointer] > nums1[left_pointer]:
                 nums1[to_place] = nums2[right_pointer]
                 right_pointer -= 1
                 to_place -= 1
-            elif right_pointer < 0:
-                break
-            elif nums1[left_pointer] >= nums2[right_pointer]:
+            else:
                 nums1[to_place] = nums1[left_pointer]
                 left_pointer -= 1
                 to_place -= 1  
-            else:
-                nums1[to_place] = nums2[right_pointer]
-                right_pointer -= 1
-                to_place -= 1
+           
         
         
