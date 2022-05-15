78ca4cd01ff7434c51192a12341fbd5bce22a2c3
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-05-15 15:04:08 -0700

    Time: 165 ms (17.48%), Space: 14.5 MB (98.90%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -1,27 +1,18 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        if r * c == len(mat[0]) * len(mat):
-            new_lst = []
-            index = 0
-            idx_counter = 0
-            for i in range(r):
-                new_lst.append([])
-
-            for i in range (len(mat)):
-                for j in range (len(mat[i])):
-                    if idx_counter < c and len(new_lst[index]) < c:
-                        new_lst[index].append(mat[i][j])
-                        idx_counter += 1
-                    elif idx_counter >= c:
-                        index += 1
-                        new_lst[index].append(mat[i][j])
-                        idx_counter = 0
-                    else:
-                        index += 1
-                        new_lst[index].append(mat[i][j])
-                        idx_counter = 0
-            mat = copy.deepcopy(new_lst)
+        if r * c != len(mat[0]) * len(mat):
             return mat
 
-        else:
-            return mat
+        res = []
+        sub = []
+
+        for i in range(len(mat)):
+            for j in range(len(mat[i])):
+
+                sub.append(mat[i][j])
+
+                if len(sub) == c:
+                    res.append(sub)
+                    sub = []
+
+        return res
