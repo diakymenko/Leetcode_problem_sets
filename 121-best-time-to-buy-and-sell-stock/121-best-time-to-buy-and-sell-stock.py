fdc54f7faa3ca600bc4fb28db6a44cdd2a7a0478
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-05-24 13:56:42 -0700

    Time: 1201 ms (66.11%), Space: 25 MB (38.58%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -8,21 +8,16 @@ class Solution:
 #                     diff = prices[j] - prices[i]
 #         return diff
         diff = 0
-        min_num = 9999999999
+        min_num = prices[0]
         max_num = 0
-        min_num_idx = 0
-        max_num_idx = 0
 
-        for idx, num in enumerate(prices):
-            if num < min_num:
-                min_num = num
-#                min_num_idx = idx
-#                max_num = 0
-#            if num > max_num and idx > min_num_idx:
-#                max_num = num
-#                max_num_idx = idx
-            if num - min_num > diff: #and min_num_idx < max_num_idx:
-                diff = num - min_num
+
+        for i in range(1, len(prices)):
+            if prices[i] < min_num:
+                min_num = prices[i]
+            if prices[i] - min_num > diff:
+                diff = prices[i] - min_num
+
         return diff
 
             
