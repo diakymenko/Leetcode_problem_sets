873c08939fea1ae3a7a6e986e62b41a6d1821842
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-06-12 21:49:21 -0700

    Time: 98 ms (36.77%), Space: 15.1 MB (66.18%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -3,11 +3,8 @@ class Solution:
         current_profit = 0
         min_value = prices[0]
 
-
         for i in range(1, len(prices)):
             if prices[i] > min_value:
                 current_profit += prices[i] - min_value
-                min_value = prices[i]
-            else:
-                min_value = prices[i]
+            min_value = prices[i]
         return current_profit
