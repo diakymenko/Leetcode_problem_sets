c648360c11396a5d43c5a19b74ed9f34f107630a
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-07-03 17:06:17 -0700

    Time: 149 ms (8.15%), Space: 13.9 MB (16.36%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -6,16 +6,15 @@ class Solution:
         elif x == 0:
             return True
         
-        new_str = ""
+        new_lst = []
         n = x
-        res = None
 
         while n > 0:
             number = n % 10
             n = n // 10
-            new_str += str(number)
+            new_lst.append(str(number))
         
-        res = int(new_str)
+        res = int("".join(new_lst))
 
         return res == x
         
