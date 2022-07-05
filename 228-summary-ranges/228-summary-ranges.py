52a662d98049de3732c0b1938890025dda2648ab
diakymenko <80910811+diakymenko@users.noreply.github.com>
2022-07-05 15:03:34 -0700

    Time: 32 ms (89.71%), Space: 13.9 MB (71.23%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -6,26 +6,22 @@ class Solution:
         count = 0
         if len(nums) == 1:
             lst.append(str(nums[0]))
-        for i in range(len(nums)-1):
-            if nums[i+1] - nums[i] == 1:
+        for i in range(len(nums) - 1):
+            if nums[i + 1] - nums[i] == 1:
                 if count == 0:
                     first = nums[i]
-                    last = nums[i+1]
+                    last = nums[i + 1]
                     count += 1
-                    if i == len(nums) - 2:
-                        lst.append(f'{first}->{last}')
                 else:
-                    last = nums[i+1]
-                    if i == len(nums) - 2:
-                        lst.append(f'{first}->{last}')
-            elif nums[i+1] - nums[i] > 1:
+                    last = nums[i + 1]
+                if i == len(nums) - 2:
+                    lst.append(f'{first}->{last}')
+            elif nums[i + 1] - nums[i] > 1:
                 if count == 0:
                     lst.append(str(nums[i]))
                 else:
                     lst.append(f'{first}->{last}')
-                    first = None
-                    last = None
-                    count = 0
+                    first,last,count = None,None,0
                 if i == len(nums) - 2:
-                    lst.append(str(nums[i+1]))
+                    lst.append(str(nums[i + 1]))
         return lst
