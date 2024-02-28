3cf0a8f39eef258abefd7db6b847cbd710c76511
diakymenko <80910811+diakymenko@users.noreply.github.com>
2024-02-28 13:17:33 -0800

    Time: 52 ms, Memory: 19 MB - LeetHub

--- linked-list-cycle/linked-list-cycle.py
+++ linked-list-cycle/linked-list-cycle.py
@@ -11,25 +11,25 @@ class Solution:
         #     return False
     
         
-#         slow = head
-#         fast = head
+        slow = head
+        fast = head
         
-#         while slow != None and fast !=None:
-#             slow = slow.next
-#             fast = fast.next.next
-#             if slow == fast:
-#                 return True
-            
-#         return False
-    
-    
-        marker1 = head
-        marker2 = head
-        while marker2!=None and marker2.next!=None:
-            marker1 = marker1.next
-            marker2 = marker2.next.next
-            if marker2==marker1:
+        while fast != None and fast.next !=None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
+            
         return False
+    
+    
+#         marker1 = head
+#         marker2 = head
+#         while marker2!=None and marker2.next!=None:
+#             marker1 = marker1.next
+#             marker2 = marker2.next.next
+#             if marker2==marker1:
+#                 return True
+#         return False
             
             
