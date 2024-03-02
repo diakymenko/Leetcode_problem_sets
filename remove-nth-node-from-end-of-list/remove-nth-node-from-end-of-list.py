481e02c0d6e53eb9ff147eac3e227ca1f76abfb6
diakymenko <80910811+diakymenko@users.noreply.github.com>
2024-03-01 18:24:59 -0800

    Time: 40 ms, Memory: 16.7 MB - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -6,29 +6,48 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-        count = 1
-        curr = head
-        lst_head = head
+#         count = 1
+#         curr = head
+#         lst_head = head
         
-        if curr.next == None and n == 1:
-            return None
+#         if curr.next == None and n == 1:
+#             return None
         
-        while curr.next != None:
-            curr = curr.next
-            count+= 1
-        print(count)
+#         while curr.next != None:
+#             curr = curr.next
+#             count+= 1
         
-        curr = head
-        idx = count - n
-        print(idx)
+#         curr = head
+#         idx = count - n
         
-        if n == count:
-            head = head.next
-        print(head.val)
-     
+#         if n == count:
+#             head = head.next
+#         print(head.val)
        
-        for i in range(idx-1):
-            curr = curr.next
-        curr.next = curr.next.next
+#         for i in range(idx-1):
+#             curr = curr.next
+#         curr.next = curr.next.next
+#         return head
+
+#two pointers
+
+        fast = head
+        slow = head
+
+        for i in range(n):
+            fast = fast.next
+
+        # This situation would happen when we are required to del the first node (n = len(List))
+            # Also, it can handle the [] case
+        if not fast:
+            return slow.next
+
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+
         return head
             
