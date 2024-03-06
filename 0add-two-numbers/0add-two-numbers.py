8ee9824983b32c9b18ac9277434cae7198672ecf
diakymenko <80910811+diakymenko@users.noreply.github.com>
2024-03-06 14:58:25 -0800

    Time: 56 ms, Memory: 16.5 MB - LeetHub

--- 0add-two-numbers/0add-two-numbers.py
+++ 0add-two-numbers/0add-two-numbers.py
@@ -22,47 +22,58 @@ class Solution:
     
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        def addNode(l3, curr3, l1, incr_number):
-            while l1:
-                sum_val = l1.val + incr_number
-                incr_number = 0
-                if sum_val > 9:
-                    incr_number = sum_val // 10
-                    sum_val = sum_val % 10
-                curr3.next = ListNode(sum_val)
-                curr3 = curr3.next
-                l1 = l1.next
-            if incr_number != 0:
-                curr3.next = ListNode(incr_number)
+#         def addNode(l3, curr3, l1, incr_number):
+#             while l1:
+#                 sum_val = l1.val + incr_number
+#                 incr_number = 0
+#                 if sum_val > 9:
+#                     incr_number = sum_val // 10
+#                     sum_val = sum_val % 10
+#                 curr3.next = ListNode(sum_val)
+#                 curr3 = curr3.next
+#                 l1 = l1.next
+#             if incr_number != 0:
+#                 curr3.next = ListNode(incr_number)
 
-            return l3.next
+#             return l3.next
         
-        l3 = ListNode() 
-        curr3 = l3
-        incr_number = 0
+#         l3 = ListNode() 
+#         curr3 = l3
+#         incr_number = 0
         
-        while l1 and l2:
-            sum_val = l1.val + l2.val + incr_number
-            incr_number = 0
-            if sum_val > 9:
-                incr_number = sum_val // 10
-                sum_val = sum_val % 10
-            curr3.next = ListNode(sum_val)
-            curr3 = curr3.next
-            l1 = l1.next
-            l2 = l2.next
+#         while l1 and l2:
+#             sum_val = l1.val + l2.val + incr_number
+#             incr_number = 0
+#             if sum_val > 9:
+#                 incr_number = sum_val // 10
+#                 sum_val = sum_val % 10
+#             curr3.next = ListNode(sum_val)
+#             curr3 = curr3.next
+#             l1 = l1.next
+#             l2 = l2.next
             
         
-        if l1:
-            return addNode(l3, curr3, l1, incr_number)
-        if l2:
-            return addNode(l3, curr3, l2, incr_number)
-        if incr_number != 0:
-            curr3.next = ListNode(incr_number)
+#         if l1:
+#             return addNode(l3, curr3, l1, incr_number)
+#         if l2:
+#             return addNode(l3, curr3, l2, incr_number)
+#         if incr_number != 0:
+#             curr3.next = ListNode(incr_number)
 
-        return l3.next
+#         return l3.next
         
-        
+        carry = 0;
+        res = n = ListNode(0);
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next;
+            if l2:
+                carry += l2.val;
+                l2 = l2.next;
+            carry, val = divmod(carry, 10) #divmo
+            n.next = n = ListNode(val);
+        return res.next;
     
 
     
