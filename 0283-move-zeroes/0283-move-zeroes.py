d1b0ca455cd85cca2274633de900846d4b164c58
diakymenko <80910811+diakymenko@users.noreply.github.com>
2024-07-23 17:15:28 -0700

    Time: 119 ms (86.53%), Space: 18.1 MB (50.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,20 +1,32 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        
         
         left = 0
-        right = 0
-        
-        while right < len(nums):
+        for right in range(len(nums)):
             if nums[right] != 0:
+                # without python magic
+                temp = nums[left]
                 nums[left] = nums[right]
+                nums[right] = temp
+
+                # python magic way to swap variables
+                # nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-            right += 1
-        while left < len(nums):
-            nums[left] = 0
-            left += 1
+        
+        
+#         left = 0
+#         right = 0
+        
+#         while right < len(nums):
+#             if nums[right] != 0:
+#                 nums[left] = nums[right]
+#                 left += 1
+#             right += 1
+#         while left < len(nums):
+#             nums[left] = 0
+#             left += 1
+            
                 
 
     
