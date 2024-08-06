0e9af9e3aa03b2484f42123b8697c5d3792b60bc
diakymenko <80910811+diakymenko@users.noreply.github.com>
2024-08-05 19:56:06 -0700

    Time: 278 ms (31.90%), Space: 26.2 MB (19.22%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,11 +7,11 @@ class Solution:
 
         for i in range(1, len(nums)):
             left_product[i] = left_product[i-1] * nums[i-1]
-        print(left_product)
+      
         
         for i in range(len(nums)-2, -1, -1):
             right_product[i] = right_product[i+1] * nums[i+1]
-        print(right_product)
+        
         
         for i in range(len(nums)):
             res.append(right_product[i] * left_product[i])
