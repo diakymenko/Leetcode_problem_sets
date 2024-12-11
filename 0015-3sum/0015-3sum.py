6447530a958443d3237e2c4e4edb4c834e4c2b70
diakymenko <80910811+diakymenko@users.noreply.github.com>
2024-12-11 15:37:25 -0800

    Time: 579 ms (59.73%), Space: 21.3 MB (7.33%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,38 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # res = []
-        # set_unique = set()
 
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         for k in range(j+1, len(nums)):
-        #             if nums[i] + nums[j] + nums[k] == 0:
-        #                 lst = [nums[i],nums[j],nums[k]]
-        #                 lst.sort()
-        #                 key = str(lst[0]) + "," + str(lst[1]) + "," + str(lst[2])
-        #                 if key not in set_unique:
-        #                     set_unique.add(key)
-        #                     res.append(lst)
-        
-        # return res
-
-        answer = set()
+        set_res = set()
         nums.sort()
-        target = 0
+
+        res = []
         for i in range(len(nums)):
-            lo = i + 1
-            hi = len(nums) -1
-            a = nums[i]
-            while lo < hi:
-                if a + nums[lo] + nums[hi] < target:
-                    # too small, increase sum
-                    lo += 1
-                elif a + nums[lo] + nums[hi] > target:
-                    # too large, decrase sum
-                    hi -= 1
+            j = i + 1
+            k = len(nums) - 1
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
                 else:
-                    # equals target
-                    answer.add(tuple([a, nums[lo], nums[hi]]))
-                    lo += 1
-                    hi -= 1
-        return answer
+                    set_res.add(tuple((sorted([nums[i], nums[j], nums[k]]))))
+                    j+= 1
+            
+        for i in set_res:
+            res.append(list(i))
+
+        return res
+
+       
